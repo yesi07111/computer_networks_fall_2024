913df5a46591514e4ac78534c82e98864843f144
rmarticedeno <rmarticedeno@gmail.com>
2024-12-13 13:27:50 -0500

    Add run script

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Change the PROTOCOL environment variable to set the execution pipeline for the given project
+# Possible values are
+# 1. HTTP
+# 2. FTP
+# 3. SMTP
+# 4. IRC
+
+PROTOCOL=0
+
+# Replace the next shell command with the entrypoint of your solution
+
+echo $@
