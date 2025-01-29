3a01ec7c9d2e697ca997a63e6f2de5fa366f7980
chriss8g <chriss.8.g@citmatel.inf.cu>
2025-01-29 08:59:50 -0500

    fix tests.py http

--- tests/http/tests.py
+++ tests/http/tests.py
@@ -2,9 +2,9 @@ import os, sys
 import json
 
 def make_request(method, path, headers=None, data=None):
-    headerstr = "" if headers is None else f" -h {headers}"
-    datastr = "" if data is None else f" -b {data}"
-    response_string = os.popen(f"run.sh -m {method} -u http://localhost:8080/{path} -h {headerstr} -d {datastr}").read()
+    headerstr = "-h {}" if headers is None else f" -h {headers}"
+    datastr = "" if data is None else f" -d {data}"
+    response_string = os.popen(f"sh run.sh -m {method} -u http://localhost:8080{path} {headerstr} {datastr}").read()
     return json.loads(response_string) # JSON con campos status, body y headers
 
 # Almacena los resultados de las pruebas
@@ -48,11 +48,11 @@ response = make_request("GET", "/secure")
 evaluate_response("GET secure without Authorization", 401, response['status'], "Authorization header missing", response['body'])
 
 print_case("GET secure with valid Authorization", "Testing GET request to '/secure' with valid authorization")
-response = make_request("GET", "/secure", headers='{"Authorization": "Bearer 12345"}')
+response = make_request("GET", "/secure", headers='{\\"Authorization\\":\\"Bearer\\ 12345\\"}')
 evaluate_response("GET secure with valid Authorization", 200, response['status'], "You accessed a protected resource", response['body'])
 
 print_case("GET secure with invalid Authorization", "Testing GET request to '/secure' with invalid authorization")
-response = make_request("GET", "/secure", headers='{"Authorization": "Bearer invalid_token"}')
+response = make_request("GET", "/secure", headers='{\\"Authorization\\":\\ \\"Bearer\\ invalid_token\\"}')
 evaluate_response("GET secure with invalid Authorization", 401, response['status'], "Invalid or missing authorization token", response['body'])
 
 # Ajuste en PUT request
@@ -82,7 +82,7 @@ print_case("Malformed POST body", "Testing POST request with malformed JSON body
 response = make_request(
     "POST",
     "/secure",
-    headers='{"Authorization": "Bearer 12345", "Content-Type": "application/json"}',
+    headers='{\\"Authorization\\":\\ \\"Bearer\\ 12345\\",\\ \\"Content-Type\\":\\ \\"application/json\\"}',
     data='{"key":}'
 )
 evaluate_response(
@@ -98,7 +98,7 @@ print_case("Malformed POST body without Authorization", "Testing POST request wi
 response = make_request(
     "POST",
     "/secure",
-    headers='{"Content-Type": "application/json"}',
+    headers='{\\"Content-Type\\":\\ \\"application/json\\"}',
     data='{"key":}'
 )
 evaluate_response(
