ef62c9a00cecad9cdafc456ac7932fcb71c4aabd
rmarticedeno <rmarticedeno@gmail.com>
2025-01-29 05:57:07 -0500

    solve port mismatch

--- tests/ftp/install.sh
+++ tests/ftp/install.sh
@@ -1,3 +1,3 @@
 ip=$(hostname -I)
-docker run -d --rm --name vsftpd -p 20-22:20-22 -p 21100-21110:21100-21110 -p 990:990 -e "PASV_ADDRESS=${ip}" -v $PWD/tests/ftp/files:/home/vsftpd/user lhauspie/vsftpd-alpine 
+docker run -d --rm --name vsftpd -p 21:21 -p 21100-21110:21100-21110 -e "PASV_ADDRESS=${ip}" -v $PWD/tests/ftp/files:/home/vsftpd/user lhauspie/vsftpd-alpine 
 echo "a new file for upload" >> tests/ftp/new.txt
--- tests/ftp/run.sh
+++ tests/ftp/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 python3 tester.py
+
+if [[ $? -ne 0 ]]; then
+  exit 1
+fi
