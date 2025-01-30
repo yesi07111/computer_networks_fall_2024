2f4218b112b69950773eec66cb62c328954ae02b
rmarticedeno <rmarticedeno@gmail.com>
2025-01-30 12:47:44 -0500

    update ftp tests

--- tests/ftp/tester.py
+++ tests/ftp/tester.py
@@ -19,16 +19,16 @@ def make_test(args, expeteted_output, error_msg):
 # /directory: 1.txt
 
 tests = [
-    ("-h localhost -p 21 -u user -p pass", ("220","230",), "Login Failed"),
-    ("-h localhost -p 21 -u user -p pass -c PWD", ("150","226",), "/ directory listing failed"),
-    ("-h localhost -p 21 -u user -p pass -c CWD -a /directory", ("250",), "change directory failed"),
-    ("-h localhost -p 21 -u user -p pass -c QUIT", ("221",), "exiting ftp server failed"),
-    ("-h localhost -p 21 -u user -p pass -c RETR -a 2.txt" , ("150","226",), "could not retrieve 2.txt file"),
-    ("-h localhost -p 21 -u user -p pass -c STOR -a tests/ftp/new.txt -b new.txt", ("150", "226",), "file new.txt upload failed"),
-    ("-h localhost -p 21 -u user -p pass -c RNFR -a 2.txt -b 3.txt", ("350", "250",), "rename from 2.txt to 3.txt failed"),
-    ("-h localhost -p 21 -u user -p pass -c DELE -a new.txt", ("250",), "delete new.txt failed"),
-    ("-h localhost -p 21 -u user -p pass -c MKD -a directory2", ("257",), "directory directory2 creation failed"),
-    ("-h localhost -p 21 -u user -p pass -c RMD -a directory2", ("250",), "directory directory2 removal failed"),
+    ("-h localhost -p 21 -u user -w pass", ("220","230",), "Login Failed"),
+    ("-h localhost -p 21 -u user -w pass -c PWD", ("150","226",), "/ directory listing failed"),
+    ("-h localhost -p 21 -u user -w pass -c CWD -a /directory", ("250",), "change directory failed"),
+    ("-h localhost -p 21 -u user -w pass -c QUIT", ("221",), "exiting ftp server failed"),
+    ("-h localhost -p 21 -u user -w pass -c RETR -a 2.txt" , ("150","226",), "could not retrieve 2.txt file"),
+    ("-h localhost -p 21 -u user -w pass -c STOR -a tests/ftp/new.txt -b new.txt", ("150", "226",), "file new.txt upload failed"),
+    ("-h localhost -p 21 -u user -w pass -c RNFR -a 2.txt -b 3.txt", ("350", "250",), "rename from 2.txt to 3.txt failed"),
+    ("-h localhost -p 21 -u user -w pass -c DELE -a new.txt", ("250",), "delete new.txt failed"),
+    ("-h localhost -p 21 -u user -w pass -c MKD -a directory2", ("257",), "directory directory2 creation failed"),
+    ("-h localhost -p 21 -u user -w pass -c RMD -a directory2", ("250",), "directory directory2 removal failed"),
 ]
 
 succeed = True
