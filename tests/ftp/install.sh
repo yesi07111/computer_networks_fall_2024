e037b78e15ef1e8530916664da71d7edca1e1b90
rmarticedeno <rmarticedeno@gmail.com>
2025-02-01 17:32:19 -0500

    use correct pasv addr

--- tests/ftp/install.sh
+++ tests/ftp/install.sh
@@ -1,3 +1,2 @@
-ip=$(hostname -I)
-docker run -d --rm --name vsftpd -p 21:21 -p 21100-21110:21100-21110 -e "PASV_ADDRESS=${ip}" -v $PWD/tests/ftp/files:/home/vsftpd/user lhauspie/vsftpd-alpine 
+docker run -d --rm --name vsftpd -p 21:21 -p 21100-21110:21100-21110 -e "PASV_ADDRESS=localhost" -v $PWD/tests/ftp/files:/home/vsftpd/user lhauspie/vsftpd-alpine 
 echo "a new file for upload" >> tests/ftp/new.txt
