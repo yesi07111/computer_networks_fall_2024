0aab02444f7899ac6fb853b8f15ce71e82609ae7
yesi07111 <yeseniavr07111@gmail.com>
2025-02-10 12:35:42 -0500

    run.sh

--- run.sh
+++ run.sh
@@ -1,63 +1,30 @@
 # # Procesa los argumentos y agrega comillas a los que no son flags
-args=()
-max_consecutive_spaces=5  # Define el máximo de espacios consecutivos permitidos
+method=""
+url=""
+headers=""
+data=""
 
-while [[ $# -gt 0 ]]; do
-    case $1 in
-        -m|-u)
-            args+=("$1" "$2")
-            shift 2
-            ;;
-        -h)
-            # Inicia la construcción del diccionario de headers
-            header=""
-            shift
-            while [[ $# -gt 0 && $1 != -* ]]; do
-                # Escapa las comillas dentro de cada header
-                key_value=$(echo "$1" | sed 's/"/\\"/g')
-                header+="$key_value, "
-                shift
-            done
-            # Elimina la última coma y espacio, y envuelve el diccionario en {}
-            header="{${header%, }}"
-
-            # Chequea y corrige dobles llaves al inicio y al final
-            if [[ $header == "{{"* ]]; then
-                header="${header#\{}"
-            fi
-            if [[ $header == *"}}" ]]; then
-                header="${header%\}}"
-            fi
-
-            args+=("-h" "\"$header\"")
-            ;;
-        -d)
-            # Inicia la construcción del argumento para -d
-            data="$2"
-            shift 2
-            consecutive_spaces=0
-
-            # Continúa agregando palabras hasta que se alcance el máximo de espacios consecutivos
-            while [[ $# -gt 0 && $consecutive_spaces -lt $max_consecutive_spaces ]]; do
-                if [[ $1 =~ ^[[:space:]]*$ ]]; then
-                    ((consecutive_spaces++))
-                else
-                    data+=" $1"
-                    consecutive_spaces=0
-                fi
-                shift
-            done
-
-            # Agrega -d y su valor completo entre comillas
-            args+=("-d" "\"$data\"")
-            ;;
-        *)
-            args+=("\"$1\"")
-            shift
-            ;;
-    esac
+while getopts ":m:u:h:d:" opt; do
+  case $opt in
+    m) method="$OPTARG" ;;
+    u) url="$OPTARG" ;;
+    h) headers="$OPTARG" ;;
+    d) data="$OPTARG" ;;
+    \?) echo "Opción inválida: -$OPTARG" >&2
+        exit 1
+        ;;
+    :) echo "La opción -$OPTARG requiere un argumento." >&2
+       exit 1
+       ;;
+  esac
 done
 
+if [ -z "$method" ] || [ -z "$url" ]; then
+  echo "Uso: $0 -m <method> -u <url> -h <headers> -d <data>"
+  exit 1
+fi
+
+python3 code/client.py -m "$method" -u "$url" -H "$headers" -d "$data"
 # Ejecuta el script de Python con los argumentos procesados
 # echo "${args[@]}"
-python code/client.py "${args[@]}"
+# python code/client.py "${args[@]}"
